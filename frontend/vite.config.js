37cd0696d0dd5cbd58ec477dded363a80d029611
José Carlos <josecarlosmf.10@gmail.com>
2026-05-27 23:09:45 -0300

    Atualização FlowBank

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -34,13 +34,13 @@ export default defineConfig({
 
         icons: [
           {
-            src: '/icon.png',
+            src: '/logo.png',
             sizes: '192x192',
             type: 'image/png'
           },
 
           {
-            src: '/icon.png',
+            src: '/logo.png',
             sizes: '512x512',
             type: 'image/png'
           }
